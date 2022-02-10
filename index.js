801ceb0f5c95570e7b8df98079a40e4daa36a034
Silver Sirp <silver.sirp@khk.ee>
2022-02-10 16:19:44 +0200

    Created form request responce with API data

--- index.js
+++ index.js
@@ -2,35 +2,48 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const fetch = require('node-fetch')
-// const {response} = require("express");
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
 const key = 'ee91cb438af3ec838d3c334f3d56c61c';
-let city = 'Tartu';
+let city = 'Tartu'
 
 app.get('/', function (req, res) {
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
-    .then((response) => {
-        return response.json()
-    })
-    .then((data) => {
-        let description = data.weather[0].description
-        let city = data.name
-        let temp = Math.round(parseFloat(data.main.temp)-273.15)
-        console.log(description);
-        console.log(city);
-        console.log(temp);
+        .then((responce) => {
+            return responce.json()
+        })
+        .then((data) => {
+            let description = data.weather[0].description
+            let city = data.name
+            let temp = Math.round(parseFloat(data.main.temp) -273.15)
+            res.render('index', {
+                description: description,
+                city: city,
+                temp: temp
+            })
+        })
+})
 
-    res.render('default', {
-        description: description,
-        city: city,
-        temp: temp
-    })
-    })
+app.post('/', function (req, res) {
+    let city= req.body.cityname
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
+        .then((responce) => {
+            return responce.json()
+        })
+        .then((data) => {
+            let description = data.weather[0].description
+            let city = data.name
+            let temp = Math.round(parseFloat(data.main.temp) -273.15)
+            res.render('index', {
+                description: description,
+                city: city,
+                temp: temp
+            })
+        })
 })
 
 app.listen(3000)
-
-
