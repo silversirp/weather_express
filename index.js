e16644cd54de499b2ace9a7de007b4a00758a53a
Silver Sirp <silver.sirp@khk.ee>
2022-02-10 11:47:37 +0200

    Ongoing... Andmete kasutamine

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const express = require('express')
 const app = express()
-
 const path = require('path')
+const fetch = require('node-fetch')
+// const {response} = require("express");
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
+const key = 'ee91cb438af3ec838d3c334f3d56c61c';
+let city = 'Tartu';
+
 app.get('/', function (req, res) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
+    .then((response) => {
+        return response.json()
+    })
+    .then((data) => {
+        console.log(data);
+    })
     res.render('default')
 })
 
